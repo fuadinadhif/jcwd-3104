593dc4f98f00838fbf3144eb5ef39ac9345307e0
fuadinadhif <nadhiffuadi@gmail.com>
2024-10-29 16:07:24 +0700

    first day

--- 01-programming-fundamental/day-1/introduction.js
+++ 01-programming-fundamental/day-1/introduction.js
@@ -1,3 +1,49 @@
-console.log("Welcome to JCWD");
-console.log("Hi");
-console.log("Another changes");
+// My first JS code!
+/* 
+This is a comment
+This one too
+This is the same
+
+
+And I also 
+*/
+// console.talk();
+/* -------------------------------- Divider; -------------------------------- */
+// console.log("Hello world!");
+// console.log(1 + 1);
+
+// const alamat = "Nadhif Fuadi";
+// console.log(alamat);
+
+/* ------------------------- Ways to Create Variable ------------------------ */
+// Global Scope
+console.log(age1);
+// console.log(age2);
+// console.log(age3);
+
+{
+  // Lv. 1 Scope
+  {
+    // Lv. 2 Scope
+    var age1 = 10;
+    let age2 = 20;
+    console.log(age3);
+    const age3 = 30;
+    {
+      // Lv. 3 Scope
+      // console.log(age3);
+    }
+  }
+}
+
+// console.log(age3);
+
+// let name = "Nadhif";
+// console.log(name);
+// name = "Arya";
+// console.log(name);
+
+// const age = 10;
+// console.log(age);
+// age = 100;
+// console.log(age);
