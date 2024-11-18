6d4ee4a559560e9b1d7bd85b23a83e323c37a04c
nadhiffuadi <nadhiffuadi@gmail.com>
2024-11-18 17:18:20 +0700

    add other materials fro day 13

--- 01-programming-fundamental/playground/playground.js
+++ 01-programming-fundamental/playground/playground.js
@@ -1,27 +1,7 @@
-var createHelloWorld = function (argumen) {
-  const argumen2 = argumen;
-  return function (argumen2) {
-    return 1;
-    return "Hello World" + argumen;
-  };
-};
-
-const hello = createHelloWorld("Not Hello", {}, [1, 2, 3]);
-console.log(hello());
-
-/* ------------------------------------ - ----------------------------------- */
-// Time - O(n)
-// 10 - 2 * 10 = 20
-// 1000 - 2 * 1000 = 2000
-
-// 10 - 10 * 10 = 1000
-// 1000 - 1000 * 1000 = 1000000
-function someFunction(array) {
-  const student = [];
-  for (let i = 0; i < 1000; i++) {
-    // ...
-    for (let i = 0; i < array.length; i++) {
-      // ...
-    }
-  }
-}
+const numbers = ["A", "B", "C", "D"];
+console.log(numbers);
+numbers.slice(0, 1);
+console.log(numbers);
+numbers.splice(-3, 0, 20, 30, 40, 50);
+console.log(numbers);
+numbers.pop();
