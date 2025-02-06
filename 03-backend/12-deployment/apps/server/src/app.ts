b3222ca63484d7bd4024d3021d7914d89be298f3
nadhiffuadi <nadhiffuadi@gmail.com>
2025-02-06 09:34:35 +0700

    add new material

---
+++ 03-backend/12-deployment/apps/server/src/app.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import prisma from "./configs/prisma";
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get("/api/v1", (req, res) => {
+  res.status(200).json({ message: "API Connected!" });
+});
+
+app.get("/api/v1/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+
+    res.status(200).json({ ok: true, data: users });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "General error. Good luck!" });
+  }
+});
+
+app.post("/api/v1/users", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+
+    await prisma.user.create({
+      data: { name: name, email: email },
+    });
+
+    res.status(201).json({ ok: true, message: "User created!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "General error. Good luck!" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.info(`Server is listening on port: ${PORT}`);
+});
